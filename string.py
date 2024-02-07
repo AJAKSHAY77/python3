0b7a4e5c7bd267cf21ecf8e5a26137152def8c03
AJAKSHAY77 <ajakshayjain11@gmail.com>
2024-02-08 00:30:31 +0530

    updated

---
+++ string.py
@@ -0,0 +1,20 @@
+name = "akshayjain"
+
+name1 = name[0:4]
+name2 = name[0:5:3]
+# print(name2)
+# print(name.upper())
+
+chai = " masala chai  "
+
+# print(chai.strip())
+# print(chai.replace("masala","ginger"))
+# print(chai)
+
+lapo = "aditya, jain, is"
+
+# print(lapo.split(","))
+# print(len(lapo))
+for letter in lapo:
+    print(letter)
+    
