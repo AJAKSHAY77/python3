cb0a4eb0e9467cde0286df2660114df557471328
AJAKSHAY77 <ajakshayjain11@gmail.com>
2024-02-10 00:03:56 +0530

    leaning dist

---
+++ dist.py
@@ -0,0 +1,29 @@
+my_dist = {
+    "name1":"akshay",
+    "name2":"aditya",
+    "name3":"sangeeta"
+}
+
+# print(my_dist)
+# print(my_dist.get("name3"))
+# print(my_dist["name1"])
+
+# if "name2" in my_dist:
+#     print("ok")
+
+# for key,value in my_dist.items():
+#     print(key,":", value)
+
+# my_dist["name2"]="gunda"
+# print(my_dist) 
+
+
+for key in my_dist:
+    print(key,my_dist[key])
+
+my_dist["name3"] ="ami"
+print(my_dist) 
+
+# my_dist.pop("name1")
+# print(my_dist)
+  
--- list.py
+++ list.py
@@ -11,3 +11,36 @@ tea_variable[1:2]=["lemon","nibbu"]
 
 #learning loops and condition
 
+# print(tea_variable)
+
+# for items in tea_variable:
+#     print(items,end="-")
+
+if "lemon" in tea_variable:
+       print("yes i have lemon")    
+
+
+# tea_variable.append("hello")
+# print(tea_variable)
+
+# tea_variable.pop()
+# print(tea_variable)
+
+# tea_variable.remove("lemon")
+# print(tea_variable)
+# print(tea_variable.pop())
+
+# tea_variable.insert(2,"jain")
+# print(tea_variable)
+
+
+# tea_variable_cpy = tea_variable.copy()
+# print(tea_variable_cpy)
+
+sqared_numbers  = [x**2 for x in range(10)]
+print(sqared_numbers)
+
+var = list(["jai",43])
+print(var)
+
+
