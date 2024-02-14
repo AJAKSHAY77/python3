8b594166344f36374e208c63a0eb8707e48d052f
AJAKSHAY77 <ajakshayjain11@gmail.com>
2024-02-15 00:13:08 +0530

    updated

--- dist.py
+++ dist.py
@@ -1,52 +1,93 @@
-my_dist = {
-    "name1":"akshay",
-    "name2":"aditya",
-    "name3":"sangeeta"
-}
+# my_dist = {
+#     "name1":"akshay",
+#     "name2":"aditya",
+#     "name3":"sangeeta"
+# }
 
-# print(my_dist)
-# print(my_dist.get("name3"))
-# print(my_dist["name1"])
+# # print(my_dist)
+# # print(my_dist.get("name3"))
+# # print(my_dist["name1"])
 
-# if "name2" in my_dist:
-#     print("ok")
+# # if "name2" in my_dist:
+# #     print("ok")
 
-# for key,value in my_dist.items():
-#     print(key,":", value)
+# # for key,value in my_dist.items():
+# #     print(key,":", value)
 
-# my_dist["name2"]="gunda"
+# # my_dist["name2"]="gunda"
+# # print(my_dist) 
+
+
+# for key in my_dist:
+#     print(key,my_dist[key])
+
+# my_dist["name3"] ="ami"
 # print(my_dist) 
 
+# # my_dist.pop("name1")
+# # print(my_dist)
 
-for key in my_dist:
-    print(key,my_dist[key])
-
-my_dist["name3"] ="ami"
-print(my_dist) 
-
-# my_dist.pop("name1")
+# my_dist.popitem()
 # print(my_dist)
 
-my_dist.popitem()
-print(my_dist)
+# # my_dist.pop()
+# # print(my_dist)
 
-# my_dist.pop()
+# del my_dist["name1"]
+# print(my_dist) 
+
+# my_dist["name1"]="akshay"
 # print(my_dist)
 
-del my_dist["name1"]
-print(my_dist) 
+# my_print_copy = my_dist.copy()
+# print(my_print_copy)
 
-my_dist["name1"]="akshay"
-print(my_dist)
-
-my_print_copy = my_dist.copy()
-print(my_print_copy)
-
-my_dist["name4"]="om"
-print(my_dist)
-print(my_print_copy)
+# my_dist["name4"]="om"
+# print(my_dist)
+# print(my_print_copy)
 
 
-del my_dist["name1"]
-print(my_dist)
-print(my_dist)
+# del my_dist["name1"]
+# print(my_dist)
+# print(my_dist)
+
+
+tea_shop = {
+
+    "chai":{
+        "masala":"spicy",
+        "ginger":"zesty"
+
+    },
+    "tea":{
+        "green":"mild",
+        "black":"strong"
+    }
+}
+
+# print(tea_shop)
+
+# print("list of chai = ", tea_shop["tea"]["black"])
+
+
+# dist1 = {"name":"aksha"}
+# print(dist1)
+
+
+sqared_num = {x:x**2 for x in range(6)}
+print(sqared_num)
+
+sqared_num.clear()
+print(sqared_num)
+print(type(sqared_num))
+
+
+
+
+
+
+
+
+
+
+
---
+++ if_else.py
@@ -0,0 +1,11 @@
+age = input("Enter your age : ")
+int_age = int(age)
+
+if (int_age < 15):
+    print("you are a child") 
+
+elif int_age>15: 
+    print("teenage")
+
+elif (int_age > 30):
+    print("budda")    
