0dda63e009f23b0095ba17e7b35eac959b26f9a0
AJAKSHAY77 <ajakshayjain11@gmail.com>
2024-02-17 00:07:44 +0530

    comp

--- prog3.py
+++ prog3.py
@@ -1,1 +1,11 @@
 # learning
+
+password = input("enter your pass : ")
+passsword_lenth = len(password)
+
+if (passsword_lenth)<= 6:
+    print("weak")
+
+
+elif (passsword_lenth)>= 10:
+    print("strong")
