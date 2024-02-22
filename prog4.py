cedb50eb52c2444685a8fdd9903d7888813ffa0a
AJAKSHAY77 <ajakshayjain11@gmail.com>
2024-02-22 23:35:47 +0530

    update

--- prog4.py
+++ prog4.py
@@ -3,4 +3,6 @@ year = 2008
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
     print("year is leap year")
 else:
-    print("this is not a leap year")    
+    print("this is not a leap year") 
+
+    #####   
